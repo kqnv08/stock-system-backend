15e341cadf93fb13cbcc225b6f63a764bad29adb
kqnv08 <cali_cemento@hotmail.com>
2023-04-01 13:49:12 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { MyResolver } from './resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      playground: true,
+      sortSchema: true,
+    }),
+  ],
+  providers:[MyResolver]
+})
+export class AppModule { }
